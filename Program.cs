0e7c24a0150b815359fac28d03572117e86ec1a8
ModinaEN <modinaen@gmail.com>
2022-12-07 11:46:57 +0300

    Использована функция

--- Program.cs
+++ Program.cs
@@ -1,18 +1,38 @@
 ﻿// Задача 27: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 Console.Write($"Даны 8 случайных чисел от 0 до 100 \n");
-int [] massiv = new int[8];
 
-for (int i = 0; i<7; i++)
+int [] massiv = new int[8];
+for (int i = 0; i<8; i++)
 {
     massiv[i] = new Random().Next(0,100);
-    Console.Write($" {massiv[i]},");
 }
-massiv[7] = new Random().Next(0,100);
-Console.Write($" {massiv[7]}\n");
-Console.Write($"Преобразуем числа в массив -> [");
-for (int i = 0; i<7; i++)
+Method();
+Console.Write($"  Преобразуем числа в массив -> [");
+Method();
+Console.WriteLine("]");
+
+
+void Method()
 {
-   Console.Write($"{massiv[i]},"); 
+    for (int i = 0; i<7; i++)
+    {
+        Console.Write($"{massiv[i]},"); 
+    }
+    Console.Write($"{massiv[7]}");
 }
-Console.Write($"{massiv[7]}]");
+
+
+//for (int i = 0; i<7; i++)
+//{
+    //massiv[i] = new Random().Next(0,100);
+    //Console.Write($" {massiv[i]},");
+//}
+//massiv[7] = new Random().Next(0,100);
+//Console.Write($" {massiv[7]}\n");
+//Console.Write($"Преобразуем числа в массив -> [");
+//for (int i = 0; i<7; i++)
+//{
+  // Console.Write($"{massiv[i]},"); 
+//}
+//Console.Write($"{massiv[7]}]");
